d426f468bc46f62b0a9633d4eddbc8664a832f84
vaibhav kumar <vaibhav.messiah@gmail.com>
2026-01-10 03:20:35 +0530

    fix: cors url

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-frontend.vercel.app"
+        "https://kassandra-eight.vercel.app/"
         ],
     allow_credentials=True,
     allow_methods=["*"],
